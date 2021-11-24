b3accc1d4602a23d6299ad0d41b06b339fe7271e
johnkaa <g.johnka@gmail.com>
2021-11-24 21:35:52 +0300

    add Game and Tournament card component

---
+++ components/Games/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="games__item">
+    <div class="games__item-img">
+      <img src="../../assets/images/games/dota-02.jpg" alt="">
+      <div class="games__item-text">{{ title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'img']
+}
+</script>
+
+<style lang="scss" scoped>
+.games__item {
+  cursor: pointer;
+  &-img {
+    position: relative;
+    img {
+      border-radius: 4px;
+    }
+  }
+  &-text {
+    position: absolute;
+    left: 50%;
+    bottom: 30px;
+    transform: translateX(-50%);
+    font-size: 30px;
+    font-weight: 700;
+    line-height: 100%;
+  }
+}
+</style>
---
+++ components/Tournaments/Card.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tournaments-card">
+    <img class="tournaments-card__img" src="~assets/images/games/dota-01.jpg" alt="">
+    <div class="tournaments-card__inner">
+      <div class="tournaments-card__title">Tournament’s name will be here, maybe, in two lines</div>
+      <div class="tournaments-card__items">
+        <div class="tournaments-card__item">
+          <div class="tournaments-card__item-title">Start</div>
+          <div class="tournaments-card__item-text">07/06</div>
+        </div>
+        <div class="tournaments-card__item">
+          <div class="tournaments-card__item-title">Mode</div>
+          <div class="tournaments-card__item-text">5v5</div>
+        </div>
+        <div class="tournaments-card__item">
+          <div class="tournaments-card__item-title">Slots</div>
+          <div class="tournaments-card__item-text">16/30</div>
+        </div>
+        <div class="tournaments-card__item">
+          <div class="tournaments-card__item-title">Pool</div>
+          <div class="tournaments-card__item-text">$ 1500</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style lang="scss" scoped>
+  .tournaments-card {
+    background-color: #151A1F;
+    max-width: 370px;
+    display: inline-block;
+    cursor: pointer;
+    &__img {
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+    }
+    &__inner {
+      padding: 30px 22px;
+    }
+    &__title {
+      font-size: 22px;
+      font-weight: 500;
+      color: #E6E8EB;
+      margin-bottom: 30px;
+    }
+    &__items {
+      display: flex;
+      justify-content: space-between;
+    }
+    &__item {
+      text-align: center;
+      &-title {
+        color: #8996A3;
+        line-height: 16px;
+      }
+    }
+  }
+</style>
