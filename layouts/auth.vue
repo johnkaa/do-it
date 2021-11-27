d226f253e6fa6c23d20fbea7ff4de0ac2432a827
johnkaa <g.johnka@gmail.com>
2021-11-27 22:16:22 +0300

    update auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,3 +1,25 @@
 <template>
-  <Nuxt />
+  <div class="auth-layout">
+    <img class="auth-layout__back" src="../assets/images/icons/back.svg" alt="" @click="$router.go(-1)">
+    <nuxt-link to="/"><img class="auth-layout__exit" src="../assets/images/icons/exit.svg" alt=""></nuxt-link>
+    <Nuxt />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  .auth-layout {
+    position: relative;
+    &__back,
+    &__exit {
+      position: absolute;
+      cursor: pointer;
+      top: 16px;
+    }
+    &__back {
+      left: 16px;
+    }
+    &__exit {
+      right: 16px;
+    }
+  }
+</style>
