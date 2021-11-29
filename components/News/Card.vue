c80d4b11a440ad8e489f2a432b36ba4628bea0f6
johnkaa <g.johnka@gmail.com>
2021-11-30 02:36:43 +0300

    add news page

---
+++ components/News/Card.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="news-card" :class="{ mainPage: mainPage, bigSize: bigSize }">
+    <img class="news-card__img" :src="img" alt="" />
+    <div class="news-card__title">{{ title }}</div>
+    <div class="news-card__text">{{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['mainPage', 'bigSize', 'img', 'title', 'text'],
+  mounted() {
+    if(this.mainPage) {
+      this.text.substr(0,50)
+      this.text = this.text + '...'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .news-card {
+    max-width: 273px;
+    background-color: #20252B;
+    &__img {
+      width: 100%;
+      height: 240px;
+      object-fit: cover;
+    }
+    &__title {
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 150%;
+      color: #F5F5F5;
+      padding: 24px 24px 0;
+    }
+    &__text {
+      font-size: 16px;
+      color: #67707A;
+      padding: 18px 24px 34px;
+    }
+  }
+  .news-card.bigSize {
+    padding-left: 208px;
+    position: relative;
+    max-width: 570px;
+    height: 328px;
+    width: 100%;
+    overflow: hidden;
+    .news-card {
+      &__img {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 208px;
+        height: 328px;
+      }
+      &__title {
+        font-size: 24px;
+        line-height: 32px;
+        padding: 24px 78px 14px 34px;
+      }
+      &__text {
+        padding: 0 78px 34px 34px;
+      }
+    }
+  }
+  .news-card.mainPage {
+    background-color: #E1E6EB;
+    border: 2px solid #E1E6EB;
+    min-width: 370px;
+    .news-card {
+      &__img {
+        width: 100%;
+        height: 198px;
+      }
+      &__title {
+        padding: 30px 22px 22px;
+        font-size: 22px;
+        line-height: 130%;
+        color: #0F1215;
+      }
+      &__text {
+        padding: 0 22px 42px;
+        color: #5E656B;
+      }
+    }
+  }
+  @media (max-width: 1300px) {
+    .news-card.bigSize {
+      max-width: 100%;
+    }
+    .news-card {
+      max-width: 48%;
+    }
+  }
+  @media (max-width: 650px) {
+    .news-card.bigSize {
+      max-width: 100%;
+      height: 100%;
+      padding: 0;
+      overflow: visible;
+      .news-card {
+        &__img {
+          position: static;
+          max-width: 100%;
+          width: 100%;
+          height: 240px;
+        }
+        &__title {
+          font-weight: 500;
+          font-size: 18px;
+          line-height: 150%;
+          color: #F5F5F5;
+          padding: 24px 24px 0;
+        }
+        &__text {
+          font-size: 16px;
+          color: #67707A;
+          padding: 18px 24px 34px;
+        }
+      }
+    }
+    .news-card {
+      max-width: 100%;
+    }
+  }
+</style>
---
+++ pages/news.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="news">
+    <div class="news__top">
+      <h2 class="news__title title">News</h2>
+      <div class="news__filter">
+        <select-categories />
+      </div>
+    </div>
+    <div class="news__items">
+      <news-card
+        class="news__item"
+        v-for="(item, index) in news"
+        :key="index"
+        :bigSize="index % 6 === 0 || index % 6 === 1"
+        :img="item.img"
+        :title="item.title"
+        :text="item.text"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    const newsRef = this.$fire.database.ref('news')
+    try {
+      const snapshot = await newsRef.once('value')
+      this.news = snapshot.val()
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  data() {
+    return {
+      news: []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .news {
+    padding-bottom: 50px;
+    &__top {
+      display: flex;
+    }
+    &__title {
+      margin-bottom: 34px;
+      margin-right: 48px;
+    }
+    &__items {
+      display: flex;
+      gap: 24px;
+      flex-wrap: wrap;
+    }
+  }
+  @media (max-width: 1300px) {
+    .news__items {
+      justify-content: space-between;
+    }
+  }
+  @media (max-width: 700px) {
+    .news__top {
+      display: block;
+      margin-bottom: 20px;
+    }
+    .news__title {
+      margin-bottom: 10px;
+    }
+  }
+</style>
